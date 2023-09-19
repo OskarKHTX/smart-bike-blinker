85d8ec30ec433bfa13f85fe3e8404224f397ebef
Oskar Kragh <49984376+OskarKHTX@users.noreply.github.com>
2023-09-19 09:31:42 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,46 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 5) {
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
+    } else if (receivedNumber == 10) {
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+radio.setGroup(122)
